838732730250423737627a1c21b4962766b84f63
Benjamin Nguyen <90567404+ben-30@users.noreply.github.com>
2021-10-27 14:00:12 -0400

    add comments

--- Microcontroller.ino
+++ Microcontroller.ino
@@ -1,26 +1,29 @@
 void setup() {
+  //bottom right pins
   pinMode(9, INPUT);
   pinMode(10, INPUT);
   pinMode(11, INPUT);
-  
+
+  //bottom left pins
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  
+
+  //top left pins
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(8, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(6, LOW);
-  
-  bool honking = false; 
-  bool turning = false;
-  bool braking = false;
+  //reset pins after each command
+  for (int pin=6; pin<=8; pin++) 
+    digitalWrite(pin, LOW);
 
+  //reset actions after each command
+  bool honking, turning, braking = false;
+
+  //time to perform command
   int timer = 20;
 
   honking = honk_it(timer);
@@ -36,7 +39,8 @@ bool honk_it(int timer) {
 
   int count = 0;
   while (count<timer) {
-    if (digitalRead(9) == HIGH) { return true; }
+    if (digitalRead(9) == HIGH) 
+      return true;
     delay(1);
     count++;
   }
@@ -49,7 +53,8 @@ bool turn_it(int timer) {
 
   int count = 0;
   while (count<timer) {
-    if (digitalRead(10) == HIGH) { return true; }
+    if (digitalRead(10) == HIGH)
+      return true;
     delay(1);
     count++;
   }
@@ -62,7 +67,8 @@ bool brake_it(int timer) {
 
   int count = 0;
   while (count<timer) {
-    if (digitalRead(11) == HIGH) { return true; }
+    if (digitalRead(11) == HIGH)
+      return true;
     delay(1);
     count++;
   }
