96bc4259e893bb5302296bf564bbb5fdf229ad6b
Benjamin Nguyen <90567404+ben-30@users.noreply.github.com>
2021-10-29 13:53:11 -0400

    change turn_it to potentiometer

--- Microcontroller.ino
+++ Microcontroller.ino
@@ -21,8 +21,14 @@ void setup() {
   pinMode(4, OUTPUT);
 }
 
+int previous_turn;
+
 void loop() {
+  //0 turned to left, 1 turned to right
+  previous_turn = (digitalRead(10) == HIGH ? 1 : 0);
+  
   //while (digitalRead(0) == HIGH) - POWER
+  
   while (true) {
 
     //if (digitalRead(1) == HIGH) - START
@@ -30,6 +36,7 @@ void loop() {
     //int score = 0
 
     digitalWrite(8, HIGH);
+
   
     //reset after each command
     bool action_success = false;
@@ -39,7 +46,7 @@ void loop() {
   
     //choose command
     //int random = rand() % 3 + 1;
-    int random = 1;
+    int random = 2;
   
     //turn yellow light on
   
@@ -90,8 +97,19 @@ bool honk_it(int timer) {
 bool turn_it(int timer) {
   int count = 0;
   while (count<timer) {
-    if (digitalRead(10) == HIGH)
-      return true;
+    switch (previous_turn) {
+      case 0:
+        if (digitalRead(10) == HIGH) {
+          previous_turn = 1;
+          return true;
+        }
+      case 1:
+        if (digitalRead(10) == LOW) {
+          previous_turn = 0;
+          return true;
+        }
+    }
+  
     delay(1);
     count++;
   }
