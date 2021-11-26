58728e4a9d5325ef70b4379609d9429e6c88d1bb
Bryan-Hess <56372299+Bryan-Hess@users.noreply.github.com>
2021-11-25 20:04:57 -0500

    Add files via upload

--- Microcontroller.ino
+++ Microcontroller.ino
@@ -1,4 +1,25 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+// If using software SPI (the default case):
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+
+#define SSD1306_LCDHEIGHT 64
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 void setup() {
+  //Pin mapping
   pinMode(0, INPUT);  //power
   pinMode(1, INPUT);  //start
   pinMode(2, OUTPUT); //green
@@ -8,8 +29,15 @@ void setup() {
   pinMode(6, INPUT);  //hand brake
   pinMode(7, INPUT);  //potentiometer
   pinMode(8, INPUT);  //push button
-  
-  //////////LCD Stuff//////////
+
+  //LCD Code
+  Serial.begin(9600);
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // init done
+
+  // Clear the buffer.
+  display.clearDisplay();
 }
 
 void loop() {
@@ -17,127 +45,175 @@ void loop() {
   //while (digitalRead(0) == HIGH) - POWER
   //if (digitalRead(1) == HIGH) - START
   //int score = 0
-  int timer = 2500;
+  int timer = 5000;
+  int score = 0;
   
-  while (true) {
+  while (true) { //Change while(true) to while(cont) 
 
     //reset after each command
+    int current_turn = 0;
+    int current_brake = 0;
+    bool honk_success = false;
+    bool turn_success = false;
+    bool brake_success = false;
     bool correct = false;
-    int current_turn = get_current_turn();
-    int current_brake = get_current_brake();
+    
+    current_turn = ( (digitalRead(7) == HIGH) ? 1 : 0 ); //Checks if pot is high or low
+
+    //Checks how the handbrake is orierneted
+    if ( digitalRead(5) == HIGH ) {
+      current_brake = 5;
+    } else if ( digitalRead(6) == HIGH ) {
+      current_brake = 6;
+    } else {
+      current_brake = 5;
+    }
   
-    //choose command --> honk = 1, turn = 2, brake = 3
-    //int num_command = rand() % 3 + 1; //random
-    int num_command = 1; //manually set for testing
+    //choose command
+    int random = rand() % 3 + 1;
+    //int random = 1;
+
+    String command = "";
+    //Command string to display 
+    switch(random){
+      case 1:
+        command = "Honk It!";
+        break;
+      case 2:
+        command = "Turn It!";
+        break;
+      case 3:
+        command = "Brake It!"; 
+        break;
+    }
+
+    // text display tests
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println(command);
+    display.setTextColor(BLACK, WHITE); // 'inverted' text
+    display.display();
+    //delay(2000);
+    
+
   
     digitalWrite(3, HIGH); //turn yellow on
+    
 
-    correct = command(timer, num_command, current_turn, current_brake);
-  
+    //Calls functions
+    int count=0;
+    while(count<timer&&!(honk_success||turn_success||brake_success)){
+      honk_success = honk_it(timer);
+      turn_success = turn_it(timer, current_turn);
+      brake_success = brake_it(timer, current_brake);
+      delay(1);
+      count++;
+    }
+    
+    //Checks if the correct input is entered and the other two have not changed
+    switch (random) {
+      case 1:
+        if ( honk_success == true ) { correct = true; }
+        if ( turn_success == true || brake_success == true ) { correct = false; }
+        break;
+      case 2:
+        if ( turn_success == true ) { correct = true; }
+        if ( honk_success == true || brake_success == true ) { correct = false; }
+        break;
+      case 3:
+        if ( brake_success == true ) { correct = true; }
+        if ( honk_success == true || turn_success == true ) { correct = false; }
+        break;
+    }
+
+    display.clearDisplay();
     digitalWrite(3, LOW); //turn yellow off
-  
-    if (correct) {
+    
+    if (correct == true) {
       digitalWrite(2, HIGH); //turn green on
+      // Sucess LCD
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("Nice Job!");
+      display.setTextColor(BLACK, WHITE); // 'inverted' text
+      display.display();
+      delay(2000);
+      display.clearDisplay();
+
+      score++;
+      timer -= 50;
       delay(1000);
-      //tell user they succeeded
-      //score++;
-      //timer--;
-      digitalWrite(2, LOW); //turn green off
-      delay(1000);
-    } else {
+    }else{
       digitalWrite(4, HIGH); //turn red on
-      delay(1000);
-      //tell user they failed
+      //Fail LED
+      // text display tests
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("You Lose!");
+      display.setTextColor(BLACK, WHITE); // 'inverted' text
+      display.display();
+      delay(2000);
+      display.clearDisplay();
+
+      display.println("Score:");
+      display.display();
+      delay(2000);
+      display.clearDisplay();
+
+      display.write(score);
+      //display.display();
+      delay(2000);
+      display.clearDisplay();
+      
       //end game
       //report score
-      digitalWrite(4, LOW); //turn red off
       delay(1000);
     }
+  
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
+    delay(1000);
   }
 
   //if (score == 99) end game;
 }
 
-//potentiometer on or off
-int get_current_turn() {
-  return (digitalRead(7) == HIGH) ? 1 : 0;
-}
-
-//brake up or down
-int get_current_brake() {
-  if ( digitalRead(5) == HIGH ) {
-    return 5;
-  } else if ( digitalRead(6) == HIGH ) {
-    return 6;
-  } else {
-    return 5;
-  }
-}
-
-int command(int timer, int num_command, int previous_turn, int previous_brake) {
+bool honk_it(int timer) {
   int count = 0;
-  
-  while (count<timer) {
-    bool did_honk = check_honk();
-    bool did_turn = check_turn(previous_turn);
-    bool did_brake = check_brake(previous_brake);
+    if (digitalRead(8) == HIGH) 
+      return true;
+  return false;
+}
 
-    //true if correct input for command
-    //false if incorrect input for command
-    switch (num_command) {
-      case 1:
-        if ( did_honk == true ) { return true; }
-        if ( did_turn == true || did_brake == true ) { return false; }
-        break;
-      case 2:
-        if ( did_turn == true ) { return true; }
-        if ( did_honk == true || did_brake == true ) { return false; }
-        break;
-      case 3:
-        if ( did_brake == true ) { return true; }
-        if ( did_honk == true || did_turn == true ) { return false; }
-        break;
+bool turn_it(int timer, int previous_turn) {
+  int count = 0;
+    switch (previous_turn) { //Chooses case if pot is truned left or right
+      case 0: //If pot was on left, check if it was turned right
+        if (digitalRead(7) == HIGH) {
+          return true;
+        }
+      case 1://If pot was on right, check if it was turned left
+        if (digitalRead(7) == LOW) {
+          return true;
+        }
     }
-    
-    delay(1);
-    count++;
-  }
-
-  return false; //time runs out
+  return false;
 }
 
-bool check_honk() {
-  return digitalRead(8) == HIGH ? true : false;
-}
-
-bool check_turn(int previous_turn) {
-  //looking for change in turn
-  switch (previous_turn) {
-      case 0:
-        if (digitalRead(7) == HIGH)
+bool brake_it(int timer, int previous_brake) {
+  int count = 0;
+    switch (previous_brake) {
+      case 6: //If switch was up, check if turned low
+        if (digitalRead(5) == HIGH) {
           return true;
-        break;
-      case 1:
-        if (digitalRead(7) == LOW)
+        }
+      case 5: //If switch was low, check if turned high
+        if (digitalRead(6) == HIGH) {
           return true;
-        break;
-   }
-
-   return false;
-}
-
-bool check_brake(int previous_brake) {
-  //looking for change in brake
-  switch (previous_brake) {
-    case 6:
-      if (digitalRead(5) == HIGH)
-        return true;
-      break;
-    case 5:
-      if (digitalRead(6) == HIGH)
-        return true;
-      break;
+        }
   }
-
   return false;
 }
